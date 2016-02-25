63250855ed8b002dbfa33888118ace6296b2664c
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-02-25 13:20:57 +0200

    Add react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-console.log('Okay. We\'re ready to go!');
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+    <div>
+        Okay. We're ready to go!
+    </div>,
+    document.getElementById('root')
+)
